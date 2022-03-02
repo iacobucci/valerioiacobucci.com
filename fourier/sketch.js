70cc7cfc906c547c9ae6ab9cba2aeff86c75ac70
iacobucci <iacobuccivalerio@gmail.com>
2022-03-02 13:52:52 +0100

    deleted:    man.txt 	modified:   sketch.js 	svg/batman.svg 	svg/semibiscroma.svg

--- fourier/sketch.js
+++ fourier/sketch.js
@@ -1,11 +1,15 @@
 var f;
 var sc = 1;
+var pause = false;
 var mx,my;
 var input;
 var file;
+var t;
+var t_delta = 0;
+var t_mult = 5;
 
-function startup(){
-	f = new Fourier(100,400);
+function startup(definition,maxtrace){
+	f = new Fourier(definition,maxtrace);
 
 	f.ps[0].c = math.complex(0,0);
 	for (let p in f.ps)
@@ -21,7 +25,7 @@ function setup() {
 	input.position(10,10);
 	input.id("inputfile");
 
-	startup();
+	startup(100,400);
 
 	mx = width/2;
 	my = height/2;
@@ -31,32 +35,30 @@ function draw() {
 	createCanvas(windowWidth, windowHeight);
 	background(0);
 	translate(mx,my); 
-    scale(1 * sc, 1 * sc);
+    scale(sc, sc);
+
+	if (!pause)
+		t_delta += 1; 
+
+	t = t_delta/(60*t_mult);
 	
-	f.show(t());
-	f.trace(t());
+	f.show(t);
+	f.trace(t);
 }
 
 function handleFile(f) {
-	// console.log(f.data);
-	
 	file = f.data.split("base64,")[1];
 	raw = atob(file);
 	data = raw.split(" d=\"")[1];
 	newpath = data.substring(data.indexOf("m"),data.indexOf('\"'));
 	path.setAttribute("d",newpath);
-	startup();
-}
-
-function t(){
-	let mult = 5;
-	return frameCount/(60*mult);
+	startup(100,400);
 }
 
 function mouseWheel(event) {
 	if (event.delta > 0)
 		sc = sc / 1.1;
-	else if (sc < 8)
+	else if (sc < 10)
 		sc = sc * 1.1;
 
 	if (sc < 1){
@@ -65,8 +67,14 @@ function mouseWheel(event) {
 		sc = 1;
 	}
 }
+
 function mouseDragged(event) {
 	mx += event.movementX;
 	my += event.movementY;
 }
 
+function keyPressed(){
+	console.log(key);
+	if (key == " ")
+		pause = !pause;
+}
