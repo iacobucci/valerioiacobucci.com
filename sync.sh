5269a564841bd854e3cea4575d4a67a78f06a1ba
iacobucci <iacobuccivalerio@gmail.com>
2023-07-02 22:13:41 +0200

    modified:   sync.sh

--- sync.sh
+++ sync.sh
@@ -5,7 +5,7 @@ HOSTNAME=$(hostname)
 if [[ $HOSTNAME == "vultr" ]];
 then
 	cd /home/valerio/valerioiacobucci.com
-	git pull github master
+	git pull github $(git rev-parse --abbrev-ref HEAD 2>/dev/null)
 	./build.sh
 	docker-compose up -d
 else
