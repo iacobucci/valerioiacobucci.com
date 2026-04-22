82f5d4307e4996bf2b43a71d1338656019ca4fe4
iacobucci <iacobuccivalerio@gmail.com>
2026-04-22 12:52:30 +0200

    auth working on development and production

--- src/auth.ts
+++ src/auth.ts
@@ -3,6 +3,10 @@ import GitHub from "next-auth/providers/github"
 
 const isProduction = process.env.NODE_ENV === "production";
 
+if (!isProduction && process.env.AUTH_URL_DEV) {
+  process.env.AUTH_URL = process.env.AUTH_URL_DEV;
+}
+
 export const { handlers, auth, signIn, signOut } = NextAuth({
   providers: [
     GitHub({
